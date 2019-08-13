60a0e9324b4f686484c21783d0f19a7e269a05c6
newive <738767136@qq.com>
2019-08-13 22:04:28 +0800

    修改package.json的homepage

---
+++ build/precache-manifest.eeecca6ab526a59c51a103d41a2c9d4d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8420884f8ba80262910118a74ee860b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "579676092632baef812e",
+    "url": "./static/css/main.c417f70b.chunk.css"
+  },
+  {
+    "revision": "2ab9eaa10fb0d603f389",
+    "url": "./static/js/2.579072b0.chunk.js"
+  },
+  {
+    "revision": "579676092632baef812e",
+    "url": "./static/js/main.45799658.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "768b54e1209a5ce8920551e604127c50",
+    "url": "./static/media/1002832.768b54e1.jpg"
+  },
+  {
+    "revision": "7100265cd4006473f657bcfae0999c5f",
+    "url": "./static/media/404_Cover.7100265c.jpg"
+  },
+  {
+    "revision": "b233f316cb97c96356d524c16126b818",
+    "url": "./static/media/818997.b233f316.jpg"
+  },
+  {
+    "revision": "4af6e42f804f34ace3bfcb79392b283b",
+    "url": "./static/media/891543.4af6e42f.png"
+  },
+  {
+    "revision": "6fc06f84a1a6b8c1f1401fbf5e5bf19a",
+    "url": "./static/media/banner.6fc06f84.jpg"
+  },
+  {
+    "revision": "f536e90c16f43e18b12a49246cc9c986",
+    "url": "./static/media/fake_validate_img.f536e90c.png"
+  }
+]);
